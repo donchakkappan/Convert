b6454f1becdca8464d2997fbbaf184453e39a3a2
donchakkappan <donchakkappan@gmail.com>
2024-06-08 00:11:30 +0530

    Added text styles and colors

---
+++ app_style_guide/src/main/kotlin/com/allutils/app_style_guide/styles/Colors.kt
@@ -0,0 +1,33 @@
+package com.allutils.app_style_guide.styles
+
+import androidx.compose.ui.graphics.Color
+
+val darkestBlue = Color(0xFF006FFD)
+val darkBlue = Color(0xFF2897FF)
+val mediumBlue = Color(0xFF6FBAFF)
+val lightBlue = Color(0xFFB4DBFF)
+val lightestBlue = Color(0xFFEAF2FF)
+
+val darkestGrey = Color(0xFFC5C6CC)
+val darkGrey = Color(0xFFD4D6DD)
+val mediumGrey = Color(0xFFE8E9F1)
+val lightGrey = Color(0xFFF8F9FE)
+val lightestGrey = Color(0xFFFFFFFF)
+
+val darkestBlack = Color(0xFF1F2024)
+val darkBlack = Color(0xFF2F3036)
+val mediumBlack = Color(0xFF494A50)
+val lightBlack = Color(0xFF71727A)
+val lightestBlack = Color(0xFF8F9098)
+
+val darkGreen = Color(0xFF298267)
+val mediumGreen = Color(0xFF3AC0A0)
+val lightGreen = Color(0xFFE7F4E8)
+
+val darkOrange = Color(0xFFE86339)
+val mediumOrange = Color(0xFFFFB37C)
+val lightOrange = Color(0xFFFFF4E4)
+
+val darkRed = Color(0xFFED3241)
+val mediumRed = Color(0xFFFF616D)
+val lightRed = Color(0xFFFFE2E5)
---
+++ app_style_guide/src/main/kotlin/com/allutils/app_style_guide/styles/Texts.kt
@@ -0,0 +1,90 @@
+package com.allutils.app_style_guide.styles
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val headingH1 = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Normal,
+    fontSize = 24.sp
+)
+
+val headingH2 = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Normal,
+    fontSize = 18.sp
+)
+
+val headingH3 = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp
+)
+
+val headingH4 = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.ExtraBold,
+    fontSize = 14.sp
+)
+
+val headingH5 = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Bold,
+    fontSize = 12.sp
+)
+
+val bodyXL = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Normal,
+    fontSize = 18.sp
+)
+
+val bodyL = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp
+)
+
+val bodyM = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp
+)
+
+val bodyS = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Normal,
+    fontSize = 16.sp
+)
+
+val bodyXS = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.Medium,
+    fontSize = 12.sp
+)
+
+val actionL = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 14.sp
+)
+
+val actionM = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 12.sp
+)
+
+val actionS = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 10.sp
+)
+
+val captionM = TextStyle(
+    fontFamily = FontFamily.Default,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 10.sp
+)
